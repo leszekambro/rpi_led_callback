d4b220f9a8b769ffcee73b0fe24b0d6c70af72e7
leszekambro <58566617+leszekambro@users.noreply.github.com>
2022-04-23 12:33:48 +0200

    Add files via upload

---
+++ pushb_callback.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button GPIO23
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)#Button GPI4
+
+GPIO.setup(18, GPIO.OUT)  #LED GPIO18
+
+global freq
+freq = 1
+
+def my_callback1(channel):
+    global freq
+    freq = 0.5
+    print("freq 0.5")
+
+def my_callback2(channel):
+    global freq
+    freq = 2
+    print("freq 2")
+    
+
+GPIO.add_event_detect(23, GPIO.RISING)
+GPIO.add_event_detect(24, GPIO.RISING)
+GPIO.add_event_callback(23, my_callback1)
+GPIO.add_event_callback(24, my_callback2)
+
+raw_input("Nacisnik enter jak bedziesz gotowy\n>")  
+try:
+    while True:
+             GPIO.output(18, True)
+             print("Led ON: ", freq)
+             time.sleep(freq)
+             GPIO.output(18, False)
+             print("Led OFF: ", freq)
+             time.sleep(freq)
+            
+except:
+    GPIO.cleanup()
